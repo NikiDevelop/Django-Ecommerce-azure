2063d85ff641c75e066cf9401eb2a494bb20e8d8
Nikifor Genchev <105102619+NikiDevelop@users.noreply.github.com>
2023-09-27 21:17:04 +0200

    Delete staticfiles/ProyectoWebApp directory

--- staticfiles/ProyectoWebApp/js/tienda.js
+++
@@ -1,6 +0,0 @@
-var botonMenu = document.getElementById('boton-menu');
-    var opcionesMenu = document.getElementById('opciones-menu');
-    
-    botonMenu.addEventListener('click', function() {
-      opcionesMenu.style.display = opcionesMenu.style.display === 'none' ? 'block' : 'none';
-    });
